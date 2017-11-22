a91515a01ab7299b960d943ca8e4ab3a1b5b0987
ShenSKY <sky@hi-global.cn>
2017-11-22 15:31:40 +0800

    测试版本

--- MusicDemoSwift/MusicDemoSwift/Controller/DetailViewController.swift
+++ MusicDemoSwift/MusicDemoSwift/Controller/DetailViewController.swift
@@ -62,7 +62,14 @@ class DetailViewController: UIViewController {
         addProgressTimer()
 //        设置歌词定时器
         addLrcTimer()
+//        状态栏设成白色
+        UIApplication.shared.statusBarStyle = .lightContent
     }
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        stopTimer(2)
+    }
+
 //    MARK: UI设置
     func setUI() {
         setUIMessageWith(model)
@@ -116,7 +123,23 @@ class DetailViewController: UIViewController {
         try? session.setCategory(AVAudioSessionCategoryPlayback)
         try? session.setActive(true)
     }
-//    MARK: 设置进度定时器
+//    MARK: 按钮方法
+    @IBAction func previousAndNextBtnAction(_ sender: UIButton) {
+        if sender.tag == 11 {
+            switchMusicWhetherNext(false)
+        }else {
+            switchMusicWhetherNext(true)
+        }
+    }
+    @IBAction func playBtnActiosn(_ sender: UIButton) {
+//        判断是否处于播放状态
+        if sender.isSelected {
+            resumeAndPauseWhtherPause(true)
+        }else {
+            resumeAndPauseWhtherPause(false)
+        }
+    }
+    //    MARK: 设置进度定时器
     func addProgressTimer() {
         progressTimer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(self.updateProgressInfo), userInfo: nil, repeats: true)
         RunLoop.main.add(progressTimer, forMode: .commonModes)
@@ -135,6 +158,129 @@ class DetailViewController: UIViewController {
     @objc func updateLrc() {
         lrcView.currentTime = audioPlayer.currentTime
     }
+//    MARK: 停止定时器
+    func stopTimer(_ flag: Int) {
+//        flag: 0.停止进度定时器 1.停止歌词定时器 2.停止所有定时器
+        switch flag {
+        case 0:
+            if (progressTimer != nil) {
+                progressTimer.invalidate()
+                progressTimer = nil
+            }
+        case 1:
+            if (lrcTimer != nil) {
+                lrcTimer.invalidate()
+                lrcTimer = nil
+            }
+        case 2:
+            if (progressTimer != nil) {
+                progressTimer.invalidate()
+                progressTimer = nil
+            }
+            if (lrcTimer != nil) {
+                lrcTimer.invalidate()
+                lrcTimer = nil
+            }
+        default:
+            break
+        }
+    }
+//    MARK: 播放暂停方法
+    func resumeAndPauseWhtherPause(_ status: Bool) {
+        playBtn.isSelected = !playBtn.isSelected
+        var info = MPNowPlayingInfoCenter.default().nowPlayingInfo
+        if status {
+            img.layer.pauseAnimate()
+            audioPlayer.pause()
+            progressTimer.pauseTimer()
+            info![MPNowPlayingInfoPropertyPlaybackRate] = 0.00001
+            info![MPNowPlayingInfoPropertyElapsedPlaybackTime] = audioPlayer.currentTime
+        }else {
+            img.layer.resumeAnimate()
+            audioPlayer.play()
+            progressTimer.resumeTimer()
+            info![MPNowPlayingInfoPropertyPlaybackRate] = 1.0
+        }
+        MPNowPlayingInfoCenter.default().nowPlayingInfo = info
+    }
+//    MARK: 切换歌曲方法
+    func switchMusicWhetherNext(_ flag: Bool) {
+        if flag {
+            if currentIndex == data.count - 1 {
+                currentIndex = 0
+            }else {
+                currentIndex = currentIndex + 1
+            }
+        }else {
+            if currentIndex == 0 {
+                currentIndex = data.count - 1
+            }else {
+                currentIndex = currentIndex - 1
+            }
+        }
+        model = data[currentIndex]
+        if (audioPlayer != nil) {
+            audioPlayer.stop()
+            audioPlayer = nil
+        }
+        setUIMessageWith(model)
+        playMusic()
+    }
+//    MARK: 锁屏控制
+    override func remoteControlReceived(with event: UIEvent?) {
+        let type = event?.subtype
+        switch type! {
+        case .remoteControlPlay:
+            //播放
+            resumeAndPauseWhtherPause(false)
+        case .remoteControlPause:
+            //暂停
+            resumeAndPauseWhtherPause(true)
+        case .remoteControlStop: break
+            //停止
+        case .remoteControlTogglePlayPause: break
+            //切换播放暂停（耳机线控）
+        case .remoteControlNextTrack:
+            //下一首
+            switchMusicWhetherNext(true)
+        case .remoteControlPreviousTrack:
+            //上一首
+            switchMusicWhetherNext(false)
+        case .remoteControlBeginSeekingBackward: break
+            //开始快退
+        case .remoteControlEndSeekingBackward: break
+            //结束快退
+        case .remoteControlBeginSeekingForward: break
+            //开始快进
+        case .remoteControlEndSeekingForward: break
+            //结束快进
+        default:
+            break
+        }
+    }
+//    MARK: 滑动事件方法
+    @IBAction func touchDown_StartChange(_ sender: UISlider) {
+        progressTimer.pauseTimer()
+    }
+    @IBAction func touchDragInside_EndChange(_ sender: UISlider) {
+        audioPlayer.currentTime = Double(sender.value) * audioPlayer.duration
+        progressTimer.resumeTimer()
+    }
+    @IBAction func valueChanged(_ sender: UISlider) {
+        let current = Double(sender.value) * audioPlayer.duration
+        currentTime.text = String.stringWithTime(time: current)
+    }
+    @IBAction func tapSlider(_ sender: UITapGestureRecognizer) {
+//        1.获取用户点击的点
+        let point = sender.location(in: sender.view)
+//        2.点击的点在进度条中的比例
+        let ratio = point.x / progress.bounds.size.width
+//        3.获取当前应播放的时间
+        let currentTime = audioPlayer.duration * Double(ratio)
+        audioPlayer.currentTime = currentTime
+//        4.更新进度信息
+        updateProgressInfo()
+    }
 }
 //    MARK: - UIScrollViewDelegate
 extension DetailViewController: UIScrollViewDelegate {
@@ -147,6 +293,12 @@ extension DetailViewController: UIScrollViewDelegate {
         lrcLabel.alpha = 1 - offsetRatio
     }
 }
+//    MARK: - AVAudioPlayerDelegate
 extension DetailViewController: AVAudioPlayerDelegate {
-    
+
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        if flag {
+            switchMusicWhetherNext(true)
+        }
+    }
 }
