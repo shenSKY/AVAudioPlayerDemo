7003c4da5bc28366e9c2e2cb2333eb0204a347b6
ShenSKY <sky@hi-global.cn>
2017-11-22 14:53:30 +0800

    未完待续

--- MusicDemoSwift/MusicDemoSwift/Category/String+ConversionTime.swift
+++ MusicDemoSwift/MusicDemoSwift/Category/String+ConversionTime.swift
@@ -10,7 +10,7 @@ import Foundation
 
 extension String {
 //    时间转字符串
-    func stringWithTime(time: TimeInterval) -> String {
+    static func stringWithTime(time: TimeInterval) -> String {
         let min = Int(time / 60)
         let second = Int(time) % 60
         return String(format: "%02ld:%02ld", min, second)
--- MusicDemoSwift/MusicDemoSwift/Controller/DetailViewController.swift
+++ MusicDemoSwift/MusicDemoSwift/Controller/DetailViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import AVFoundation
+import MediaPlayer
 
 class DetailViewController: UIViewController {
     
@@ -15,7 +16,7 @@ class DetailViewController: UIViewController {
     var data: [MusicModel]!
     var currentIndex: NSInteger!
 //    歌名
-    @IBOutlet weak var MusicName: UILabel!
+    @IBOutlet weak var musicName: UILabel!
 //    歌者
     @IBOutlet weak var singer: UILabel!
 //    图片
@@ -39,26 +40,113 @@ class DetailViewController: UIViewController {
 //    歌词定时器
     var lrcTimer: CADisplayLink!
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        becomeFirstResponder()
+        UIApplication.shared.beginReceivingRemoteControlEvents()
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        resignFirstResponder()
+        UIApplication.shared.endReceivingRemoteControlEvents()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+//        UI设置
+        setUI()
+//        播放音乐
+        playMusic()
+//        后台播放
+        playingBackground()
+//        设置进度定时器
+        addProgressTimer()
+//        设置歌词定时器
+        addLrcTimer()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+//    MARK: UI设置
+    func setUI() {
+        setUIMessageWith(model)
+        img.layer.masksToBounds = true
+        img.layer.cornerRadius = CGFloat(UIScreen.main.bounds.size.width * 0.8 / 2)
+        lrcView.contentSize = CGSize(width: view.bounds.size.width * 2, height: 0)
+        lrcView.lrcLabel = lrcLabel
+        lrcView.delegate = self
+        lrcView.isPagingEnabled = true
+        progress.setThumbImage(UIImage(named: "player_slider_playback_thumb"), for: .normal)
     }
+//    MARK: 设置相关信息
+    func setUIMessageWith(_ model: MusicModel) {
+        musicName.text = model.name
+        singer.text = model.singer
+        img.image = UIImage.init(named: model.icon)
+        lrcView.lrcName = model.lrcname
+    }
+//    MARK: 播放音乐
+    func playMusic() {
+        audioPlayer = try? AVAudioPlayer(contentsOf: Bundle.main.url(forResource: model.filename, withExtension: nil)!)
+        audioPlayer.delegate = self
+        audioPlayer.prepareToPlay()
+        if !audioPlayer.isPlaying {
+            audioPlayer.play()
+        }
+//        获取音乐信息
+        currentTime.text = String.stringWithTime(time: audioPlayer.currentTime)
+        totalTIme.text = String.stringWithTime(time: audioPlayer.duration)
+//        图片旋转动画
+        img.layer.startAnimate()
+        playBtn.isSelected = true
+//        设置锁屏信息显示
+        setLockScreenDisplay()
+    }
+//    MARK: 设置锁屏信息显示
+    func setLockScreenDisplay() {
+        var info = Dictionary<String, Any>()
+        info[MPMediaItemPropertyTitle] = model.name//歌名
+        info[MPMediaItemPropertyArtist] = model.singer//作者
+//        [info setObject:self.model.filename forKey:MPMediaItemPropertyAlbumTitle];//专辑名
+        info[MPMediaItemPropertyAlbumArtist] = model.singer//专辑作者
+        info[MPMediaItemPropertyArtwork] = MPMediaItemArtwork(image: UIImage.init(named: model.icon)!)//显示的图片
+        info[MPMediaItemPropertyPlaybackDuration] = audioPlayer.duration//总时长
+        info[MPNowPlayingInfoPropertyPlaybackRate] = 1.0//播放速率
+        MPNowPlayingInfoCenter.default().nowPlayingInfo = info
+    }
+//    MARK: 后台播放
+    func playingBackground() {
+        let session = AVAudioSession.sharedInstance()
+        try? session.setCategory(AVAudioSessionCategoryPlayback)
+        try? session.setActive(true)
+    }
+//    MARK: 设置进度定时器
+    func addProgressTimer() {
+        progressTimer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(self.updateProgressInfo), userInfo: nil, repeats: true)
+        RunLoop.main.add(progressTimer, forMode: .commonModes)
+    }
+//    MARK: 更新时间进度
+    @objc func updateProgressInfo() {
+        currentTime.text = String.stringWithTime(time: audioPlayer.currentTime)
+        progress.value = Float(audioPlayer.currentTime / audioPlayer.duration)
+    }
+//    MARK: 设置歌词定时器
+    func addLrcTimer() {
+        lrcTimer = CADisplayLink(target: self, selector: #selector(self.updateLrc))
+        lrcTimer.add(to: RunLoop.main, forMode: .commonModes)
+    }
+//    MARK: 更新歌词
+    @objc func updateLrc() {
+        lrcView.currentTime = audioPlayer.currentTime
+    }
+}
+//    MARK: - UIScrollViewDelegate
+extension DetailViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+//        取出scrollView偏移量
+        let Offset: CGPoint = scrollView.contentOffset
+        let offsetRatio: CGFloat = Offset.x / scrollView.bounds.size.width
+//        设置iconView和歌词Label的透明度
+        img.alpha = 1 - offsetRatio
+        lrcLabel.alpha = 1 - offsetRatio
+    }
+}
+extension DetailViewController: AVAudioPlayerDelegate {
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
